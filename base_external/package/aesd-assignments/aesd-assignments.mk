c3b066ed098ec309139ca544c7305343c59a44be
Andrew Chang <andrew99154@gmail.com>
2026-06-14 21:34:12 +0800

    Add buildroot submodule and base_external package structure
    - Add buildroot as git submodule at tag 2024.02.13 (2024.02.x series)
    - Add base_external/Config.in, external.mk, external.desc with project_base name
    - Update aesd-assignments.mk with SSH URL for assignment 3 repo
    - Add clean.sh script (runs make distclean in buildroot)
    - TODO: Set AESD_ASSIGNMENTS_VERSION in aesd-assignments.mk to actual commit hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,12 +5,12 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+# TODO: Replace with the full 40-char SHA of the HEAD commit from
+#       git@github.com:cu-ecen-aeld/assignments-3-and-later-Andrushika.git
+#       Run: git ls-remote git@github.com:cu-ecen-aeld/assignments-3-and-later-Andrushika.git HEAD
+AESD_ASSIGNMENTS_VERSION = 'COMMIT_HASH_HERE'
+# Note: ssh URL required (not https) for automated build/test system
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Andrushika.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -18,11 +18,12 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-test.sh $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Runs buildroot distclean to remove all build artifacts
+make -C buildroot distclean
