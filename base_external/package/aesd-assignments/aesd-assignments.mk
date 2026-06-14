8ff65f7c4206833f1f96aa5ed07efe9deb5a0fbc
Andrew Chang <andrew99154@gmail.com>
2026-06-14 21:41:53 +0800

    Set aesd-assignments version and fix install paths
    - Pin AESD_ASSIGNMENTS_VERSION to commit 9e2a4c7 (finder-test.sh PATH fix)
    - Install writer, finder.sh, finder-test.sh from finder-app/ to /usr/bin/
    - Install conf files to /etc/finder-app/conf/

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,10 +5,7 @@
 #
 ##############################################################
 
-# TODO: Replace with the full 40-char SHA of the HEAD commit from
-#       git@github.com:cu-ecen-aeld/assignments-3-and-later-Andrushika.git
-#       Run: git ls-remote git@github.com:cu-ecen-aeld/assignments-3-and-later-Andrushika.git HEAD
-AESD_ASSIGNMENTS_VERSION = 'COMMIT_HASH_HERE'
+AESD_ASSIGNMENTS_VERSION = 9e2a4c752427f8ad38795da5cb0ae6425bb60c08
 # Note: ssh URL required (not https) for automated build/test system
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Andrushika.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
@@ -22,8 +19,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-test.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
