5cbdc106a03cb11eb9197431b921f3ae9b00e420
Andrew Chang <andrew99154@gmail.com>
2026-06-15 20:34:09 +0800

    Update aesd-assignments version to 92f220a

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = ccf74cf56e8a83806a78015d40e6f6273602fced
+AESD_ASSIGNMENTS_VERSION = 92f220a51380587c5095eabb45474cdb9dae7855
 # Note: ssh URL required (not https) for automated build/test system
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Andrushika.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
